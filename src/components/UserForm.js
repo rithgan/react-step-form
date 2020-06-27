cdf5b5c355597a78b6d6e42754bfbd9a4c027c29
rithgan <rithikgandhi1989@gmail.com>
2020-06-27 13:53:04 +0530

    Useform stateful component

---
+++ src/components/UserForm.js
@@ -0,0 +1,88 @@
+//Stateful component
+import React, { Component } from "react";
+import FormUserDetails from "./FormUserDetails";
+import FormPersonalDetails from "./FormPersonalDetails";
+import Confirm from "./Confirm";
+import Success from "./Success";
+class UserForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      step: 1,
+      firstName: "",
+      lastName: "",
+      email: "",
+      occupation: "",
+      city: "",
+      bio: "",
+    };
+  }
+  //Proceed to next step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1,
+    });
+  };
+  //Preceed to previos step
+  previousStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1,
+    });
+  };
+
+  //Handle fields change
+  handleChange = (input) => (e) => {
+    this.setState({ [input]: e.target.value });
+  };
+
+  render() {
+    const { step } = this.state;
+    const { firstName, lastName, email, occupation, city, bio } = this.state;
+    const values = {
+      firstName,
+      lastName,
+      email,
+      occupation,
+      city,
+      bio,
+    };
+
+    switch (step) {
+      case 1:
+        return (
+          <FormUserDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return (
+          <FormPersonalDetails
+            nextStep={this.nextStep}
+            previousStep={this.previousStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 3:
+        return (
+          <Confirm
+            nextStep={this.nextStep}
+            previousStep={this.previousStep}
+            values={values}
+          />
+        );
+
+      case 4:
+        return <Success />;
+      default:
+        return null;
+    }
+  }
+}
+
+export default UserForm;
